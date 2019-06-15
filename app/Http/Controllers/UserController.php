c3832ee36d1ade0b7d44a9dabf88705834a70671
hoanganhd-1958 <doan.hoang.anh@sun-asterisk.com>
2019-06-15 16:38:54 +0700

    Store function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,8 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dc chua
+        return $request->all();
     }
 
     /**
